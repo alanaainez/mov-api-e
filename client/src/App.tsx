23bed1f412587a90c2fd87d9f30ac1f0bef5113b
Justin Hill <justinrichardhill@gmail.com>
2025-02-01 18:07:55 -0500

    Basic search bar + favorites component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddFavourite';
 import './index.css';
 
 function App() {
-  
-  const [movies] = useState([        {
-      "Title": "Star Wars: Episode IV - A New Hope",
-      "Year": "1977",
-      "imdbID": "tt0076759",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode V - The Empire Strikes Back",
-      "Year": "1980",
-      "imdbID": "tt0080684",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode VI - Return of the Jedi",
-      "Year": "1983",
-      "imdbID": "tt0086190",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  }]);
+    const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+    const [favourites, setFavourites] = useState([]);
+
+    const getMovieRequest = async () => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+    
+        const response = await fetch(url);
+        const responseJson = await response.json();
+    
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
+        }
+    };
+    
+    const addFavouriteMovie = (movie) => {
+        const newFavouriteList = [...favourites, movie];
+        setFavourites(newFavouriteList);
+    };
+    
+    useEffect(() => {
+        getMovieRequest(searchValue);
+    }, [searchValue]);
 
   return (
     <div>
       <Navbar />
-      <main className='w-full max-w-1/3 flex pt-5 px-5'>
-        <Outlet />
-          <MovieList movies={movies} />
+      <main className='w-full flex pt-5 px-5'>
+        {/* <Outlet /> */}
+          <div className='w-full flex pt-5 px-5'>
+            <MovieListHeading heading='Movies' />
+            <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+          </div>
+          <section className='w-full flex pt-5 px-5'>
+            <MovieList movies={movies} favouriteComponent={AddFavourite} handleFavouritesClick={addFavouriteMovie} />
+          </section>
       </main>
     </div>
   )
--- client/src/components/MovieList.tsx
+++ client/src/components/MovieList.tsx
@@ -1,11 +1,18 @@
 // import React from 'react';
 
 const MovieList = (props: any) => {
+    const FavouriteComponent = props.favouriteComponent;
     return (
         <>
-            {props.movies.map((movie: any) => (
-                <div className='image-container d-flex justify-content-start m-3'>
-                    <img src={movie.Poster} alt='movie'></img>
+            {props.movies.map((movie: any, index) => (
+                <div className='image-container object-contain h-full justify-start mx-1' key={index}>
+                    <img src={movie.Poster} alt='Movie poster'></img>
+                    <div
+                        onClick={() => props.handleFavouritesClick(movie)}
+                        className='overlay d-flex align-items-center justify-content-center'
+                    >
+                        <FavouriteComponent />
+                    </div>
                 </div>
             ))}
         </>
