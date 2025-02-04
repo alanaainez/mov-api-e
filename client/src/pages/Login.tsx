ffc19a24290837b78c1c040994c74dd23b8ac10c
alanaainez <alainez649@gmail.com>
2025-02-03 22:20:32 -0500

    Updated color scheme and added login styling

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -36,7 +36,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center p-7 m-20 border-solid border-3 w-100">
       {" "}
       <form className="form login-form" onSubmit={handleSubmit}>
         <h1>Who are you?</h1>
@@ -64,7 +64,7 @@ const Login = () => {
         </div>
         {/* Submit button for the login form */}
         <div className="form-group">
-          <button className="btn btn-primary" type="submit">
+          <button className="btn btn-primary bg-orange-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
             Login
           </button>
         </div>
