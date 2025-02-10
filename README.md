baa8159ceef65ef97d73daa267e08731c9ccdf7f
alanaainez <alainez649@gmail.com>
2025-02-10 18:42:42 -0500

    git commit -m "Added User Story and began 'Built With' in README"

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mov-api-e
 A web application to browse and curate movie lists.
+
+## User Story
+As a user I am able to: 
+    * search or browse a catalog of movies
+    * add a movie to my own custom list of favorites and movies Watchlist
+    * log into my own account
+
+
+## Built With
+* Tailwind
+* React
--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -5,7 +5,7 @@ const MovAPIeLogo = () => {
     <div className="flex items-center justify-center p-4">
       <div className="relative flex items-center">
         {/* Movie Ticket Style Frame */}
-        <div className="relative flex items-center border-4 border-orange-900 rounded-lg p-4 shadow-lg bg-stone-950">
+        <div className="relative flex items-center border-4 border-orange-600 rounded-lg p-4 shadow-lg bg-stone-950">
           {/* MOV Text */}
           <span className="text-yellow-100 text-5xl font-bold tracking-wide">Mov</span>
           
@@ -16,7 +16,6 @@ const MovAPIeLogo = () => {
           <div className="flex items-center">
             <span className="text-yellow-100 text-5xl font-bold">e</span>
             <div className="w-8 h-5 border-2 border-red-600 bg-orange-900 flex justify-between px-1 ml-1">
-              <div className="w-1 h-4 bg-yellow-100"></div>
               <div className="w-1 h-4 bg-yellow-100"></div>
               <div className="w-1 h-4 bg-yellow-100"></div>
             </div>
